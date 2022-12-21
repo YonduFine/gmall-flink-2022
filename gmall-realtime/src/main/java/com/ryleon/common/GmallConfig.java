f5b7363f65039ac36d63f19d60cb5bbaddb018f3
ryl <renyouliang69@163.com>
2022-12-21 19:32:57 +0800

    常量类

---
+++ gmall-realtime/src/main/java/com/ryleon/common/GmallConfig.java
@@ -0,0 +1,13 @@
+package com.ryleon.common;
+
+public class GmallConfig {
+    // Phoenix库名
+    public static final String HBASE_SCHEMA = "GMALL2212_REALTIME";
+
+    // Phoenix驱动
+    public static final String PHOENIX_DRIVER = "org.apache.phoenix.jdbc.PhoenixDriver";
+
+    // Phoenix连接参数
+    public static final String PHOENIX_SERVER = "jdbc:phoenix:bigdata101,bigdata102,bigdata103:2181";
+
+}
