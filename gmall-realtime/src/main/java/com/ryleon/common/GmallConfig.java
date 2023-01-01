80f5f0bb2dd16c7945af2d5c4bb0bed12dfce238
ryl <renyouliang69@163.com>
2023-01-01 16:36:44 +0800

    更新Clickhouse配置

--- gmall-realtime/src/main/java/com/ryleon/common/GmallConfig.java
+++ gmall-realtime/src/main/java/com/ryleon/common/GmallConfig.java
@@ -1,13 +1,32 @@
 package com.ryleon.common;
 
+/**
+ * @author Lenovo
+ */
 public class GmallConfig {
-    // Phoenix库名
+    /**
+     * Phoenix库名
+     */
     public static final String HBASE_SCHEMA = "GMALL2212_REALTIME";
 
-    // Phoenix驱动
+    /**
+     * Phoenix驱动
+     */
     public static final String PHOENIX_DRIVER = "org.apache.phoenix.jdbc.PhoenixDriver";
 
-    // Phoenix连接参数
+    /**
+     * Phoenix连接参数
+     */
     public static final String PHOENIX_SERVER = "jdbc:phoenix:bigdata101,bigdata102,bigdata103:2181";
+
+    /**
+     * ClickHouse 驱动
+     */
+    public static final String CLICKHOUSE_DRIVER = "ru.yandex.clickhouse.ClickHouseDriver";
+
+    /**
+     * ClickHouse 连接URL
+     */
+    public static final String CLICKHOUSE_URL = "jdbc:clickhouse://bigdata101:8123/gmall_2212";
 
 }
