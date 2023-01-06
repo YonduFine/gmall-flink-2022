56281f2132cb2338071286a076a354383b77ccb8
ryl <renyouliang69@163.com>
2023-01-06 17:47:10 +0800

    任务统一启动入口

---
+++ gmall-realtime/src/main/java/com/ryleon/RealFlinkStartApp.java
@@ -0,0 +1,13 @@
+package com.ryleon;
+
+/**
+ * <p>任务统一启动入口</p>
+ *
+ * @author ALiang
+ * @date 2023-01-06
+ */
+public class RealFlinkStartApp {
+    public static void main(String[] args) {
+
+    }
+}
