b22c68d4e568bd6ebb169334f6215f00141830dc
ryl <renyouliang69@163.com>
2023-01-06 17:37:30 +0800

    增加了一些转换方法

--- gmall-realtime/src/main/java/com/ryleon/util/DateFormatUtil.java
+++ gmall-realtime/src/main/java/com/ryleon/util/DateFormatUtil.java
@@ -36,4 +36,34 @@ public class DateFormatUtil {
         return dtfFull.format(localDateTime);
     }
 
+    /**
+     * <p>返回指定天对应的秒数</p>
+     *
+     * @param day 天数
+     * @return int
+     */
+    public static int days(int day) {
+        return day * 24 * 60 * 60;
+    }
+
+    /**
+     * <p>返回指定小时的秒数</p>
+     *
+     * @param hour 小时
+     * @return int
+     */
+    public static int hours(int hour) {
+        return hour * 60 * 60;
+    }
+
+    /**
+     * <p>返回指定分钟的秒数</p>
+     *
+     * @param minute 分钟
+     * @return int
+     */
+    public static int minutes(int minute) {
+        return minute * 60;
+    }
+
 }
