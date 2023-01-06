38ad0bdd9dbe5b3405d8c4ab3cc2766f8f3f1887
ryl <renyouliang69@163.com>
2023-01-06 17:37:59 +0800

    dws clickhouse建表SQL

--- gmall-realtime/src/main/resources/dws_clickhouse_create.sql
+++ gmall-realtime/src/main/resources/dws_clickhouse_create.sql
@@ -97,3 +97,80 @@ create table if not exists gmall_2212.dws_trade_payment_suc_window
 ) engine = ReplacingMergeTree(ts)
       partition by toYYYYMMDD(stt)
       order by (stt, edt);
+
+-- 8.交易域下单各窗口汇总表--统计当日下单独立用户数和新增下单用户数
+drop table if exists gmall_2212.dws_trade_order_window;
+create table if not exists gmall_2212.dws_trade_order_window
+(
+    stt                          DateTime,
+    edt                          DateTime,
+    order_unique_user_count      UInt64,
+    order_new_user_count         UInt64,
+    order_activity_reduce_amount Decimal(38, 20),
+    order_coupon_reduce_amount   Decimal(38, 20),
+    order_origin_total_amount    Decimal(38, 20),
+    ts                           UInt64
+) engine = ReplacingMergeTree(ts)
+      partition by toYYYYMMDD(stt)
+      order by (stt, edt);
+
+-- 9.SPU粒度下单各窗口汇总表
+drop table if exists gmall_2212.dws_trade_user_spu_order_window;
+create table if not exists gmall_2212.dws_trade_user_spu_order_window
+(
+    stt            DateTime,
+    edt            DateTime,
+    trademark_id   String,
+    trademark_name String,
+    category1_id   String,
+    category1_name String,
+    category2_id   String,
+    category2_name String,
+    category3_id   String,
+    category3_name String,
+    user_id        String,
+    spu_id         String,
+    spu_name       String,
+    order_count    UInt64,
+    order_amount   Decimal(38, 20),
+    ts             UInt64
+) engine = ReplacingMergeTree(ts)
+      partition by toYYYYMMDD(stt)
+      order by (stt, edt, spu_id, spu_name, user_id);
+
+-- 10.交易域省份粒度下单各窗口汇总表
+drop table if exists gmall_2212.dws_trade_province_order_window;
+create table if not exists gmall_2212.dws_trade_province_order_window
+(
+    stt           DateTime,
+    edt           DateTime,
+    province_id   String,
+    province_name String,
+    order_count   UInt64,
+    order_amount  Decimal(38, 20),
+    ts            UInt64
+) engine = ReplacingMergeTree(ts)
+      partition by toYYYYMMDD(stt)
+      order by (stt, edt, province_id);
+
+-- 11.交易域品牌-品类-用户粒度退单各窗口汇总表
+drop table if exists gmall_2212.dws_trade_trademark_category_user_refund_window;
+create table if not exists gmall_2212.dws_trade_trademark_category_user_refund_window
+(
+    stt            DateTime,
+    edt            DateTime,
+    trademark_id   String,
+    trademark_name String,
+    category1_id   String,
+    category1_name String,
+    category2_id   String,
+    category2_name String,
+    category3_id   String,
+    category3_name String,
+    user_id        String,
+    refund_count   UInt64,
+    ts             UInt64
+) engine = ReplacingMergeTree(ts)
+      partition by toYYYYMMDD(stt)
+      order by (stt, edt, trademark_id, trademark_name, category1_id,
+                category1_name, category2_id, category2_name, category3_id, category3_name, user_id);
